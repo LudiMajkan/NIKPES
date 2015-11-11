a58b4a06491ad3db2c1449b03c742bba990e8f58
LudiMajkan <nenaddragisic@gmail.com>
2015-11-11 20:28:23 +0100

    sredjivan red, cini mi se da sad radi kako treba

--- Project9/Aggregator/Aggregator.cpp
+++ Project9/Aggregator/Aggregator.cpp
@@ -9,12 +9,20 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	ThreadSafeQueue<int> *q = new ThreadSafeQueue<int>();
 
-	q->Enqueue(0x43434343);
-	//q->Enqueue(0x91919191);
-	q->Dequeue();
-	//q->Dequeue();
+	q->Enqueue(1);
+	q->Enqueue(2);
+	q->Enqueue(3);
+	q->Enqueue(4);
 
-
+	int a = q->Dequeue();
+	int b = q->Dequeue();
+	int c = q->Dequeue();
+	q->Enqueue(5);
+	q->Enqueue(6);
+	a = q->Dequeue();
+	b = q->Dequeue();
+	c = q->Dequeue();
+	q->~ThreadSafeQueue();
 	return 0;
 }
 
--- Project9/Common/ThreadSafeQueue.h
+++ Project9/Common/ThreadSafeQueue.h
@@ -7,6 +7,97 @@
 template <class T>
 class ThreadSafeQueue
 {
+public:
+	ThreadSafeQueue(T* dataToCopy, int size)
+	{
+		InitializeCriticalSection(&criticalSection);
+
+		if (newCapacity >= capacity)
+		{
+			capacity = newCapacity;
+		}
+
+		data = new T[capacity];
+		for (int i = 0; i < newCapacity; i++)
+		{
+			Enqueue(dataToCopy[i]);
+		}
+	}
+	ThreadSafeQueue(int initialSize)
+	{
+		InitializeCriticalSection(&criticalSection);
+
+		data = new T[initialSize];
+		capacity = initialSize;
+	}
+	ThreadSafeQueue()
+	{
+		InitializeCriticalSection(&criticalSection);
+
+		data = new T[INITIAL_SIZE];
+	}
+
+	~ThreadSafeQueue()
+	{
+		DeleteCriticalSection(&criticalSection);
+
+		delete(data);
+	}
+
+	void Enqueue(T newData)
+	{
+		EnterCriticalSection(&criticalSection);
+
+		tail = (tail + 1) % capacity;
+
+		if (tail == head && count > 0)
+		{
+			Resize(capacity * 2);
+		}
+
+		data[tail] = newData;
+		count++;
+		LeaveCriticalSection(&criticalSection);
+	}
+	T Dequeue()
+	{
+		T retVal = NULL;
+
+		if (count > 0)
+		{
+			EnterCriticalSection(&criticalSection);
+
+			retVal = data[head];
+			if (count > 1)
+			{
+				head = (head + 1) % capacity;
+			}
+
+			if (head < tail)
+			{
+				if (tail - head + 1 <= capacity / 4)
+				{
+					Resize(capacity / 2);
+				}
+			}
+			else if (head > tail)
+			{
+				if (capacity - head + tail + 1 <= capacity / 4) // PROVERI OVO!!!!!
+				{
+					Resize(capacity / 2);
+				}
+			}
+			else
+			{
+				// Ovo je samo za probavanje koda...
+				//cout << "Queue is empty";
+			}
+			count--;
+			LeaveCriticalSection(&criticalSection);
+		}
+		return retVal;
+	}
+
 protected:
 	T *data;
 	int count = 0;
@@ -15,7 +106,7 @@ protected:
 	int tail = -1;
 
 	CRITICAL_SECTION criticalSection;
-	
+
 	void Resize(int newCapacity)
 	{
 		if (newCapacity > INITIAL_SIZE)
@@ -57,94 +148,6 @@ protected:
 		}
 	}
 
-
-public:
-	ThreadSafeQueue(T* dataToCopy, int size)
-	{
-		InitializeCriticalSection(&criticalSection);
-
-		if (newCapacity >= capacity)
-		{
-			capacity = newCapacity;
-		}
-
-		data = new T[capacity];
-		for (int i = 0; i < newCapacity; i++)
-		{
-			Enqueue(dataToCopy[i]);
-		}
-	}
-	ThreadSafeQueue(int initialSize)
-	{
-		InitializeCriticalSection(&criticalSection);
-
-		data = new T[initialSize];
-		capacity = initialSize;
-	}
-	ThreadSafeQueue()
-	{
-		InitializeCriticalSection(&criticalSection);
-
-		data = new T[INITIAL_SIZE];
-	}
-
-	~ThreadSafeQueue()
-	{
-		DeleteCriticalSection(&criticalSection);
-
-		delete(data);
-	}
-
-	void Enqueue(T newData)
-	{
-		EnterCriticalSection(&criticalSection);
-		
-		tail = (tail + 1) % capacity;
-		// Brljavi pri prvom dodavanju...
-		if (tail == head)
-		{
-			Resize(capacity * 2);
-		}
-
-		data[tail] = newData;
-		count++;
-		LeaveCriticalSection(&criticalSection);
-	}
-	T Dequeue()
-	{
-		EnterCriticalSection(&criticalSection);
-
-		T retVal = data[head];
-		if (count > 1)
-		{
-			head = (head + 1) % capacity;
-		}
-
-		if (head < tail)
-		{
-			if (tail - head + 1 <= capacity / 4)
-			{
-				Resize(capacity / 2);
-			}
-		}
-		else if (head > tail)
-		{
-			if (capacity - head + tail + 1 <= capacity / 4) // PROVERI OVO!!!!!
-			{
-				Resize(capacity / 2);
-			}
-		}
-		else
-		{
-			// Ovo je samo za probavanje koda...
-			//cout << "Queue is empty";
-		}
-		count--;
-		LeaveCriticalSection(&criticalSection);
-
-		return retVal;
-	}
-	//void Clear();
 
 };
 
